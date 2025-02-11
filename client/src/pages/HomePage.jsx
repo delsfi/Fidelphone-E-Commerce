75008245caf3dcecd966453b32679943114d37e1
delsfi <aloisiusfidel1@gmail.com>
2025-02-11 18:17:54 +0700

    set up

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+
+
+export default function HomePage() {
+    return (
+        <div className="bg-gray-100 h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-gray-800">Welcome to Vite + React + Tailwind CSS</h1>
+        </div>
+    );
+    }
